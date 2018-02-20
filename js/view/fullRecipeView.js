5a28afe0c8133ec98dc145b3df3b8eef81d70b7b
InzyWong <vwong@kth.se>
2018-02-20 23:45:50 +0100

    small appearance fixes

--- js/view/fullRecipeView.js
+++ js/view/fullRecipeView.js
@@ -33,7 +33,7 @@ var FullReceipeView = function (container, model){
       
       for(var i=0; i< dishesSelected.length; i++)
       {
-         container.append("<div class='row'> <div class='col-md-3'> <img src='images/" + dishesSelected[i].image + "' style='width:150px; height: 150px'> </div> <div class='col-md-4 bg-primary'> <h5>" + dishesSelected[i].name + "</h5> <p>" + dishesSelected[i].description + "</p> </div>  <div class='col-md-5 bg-success'> <h5>Preperations</h5> <p>" + dishesSelected[i].description + "</p> </div> </div>");
+         container.append("<div class='row'> <div class='col-md-3'> <img src='images/" + dishesSelected[i].image + "' style='width:150px; height: 150px'> </div> <div class='col-md-4'> <h5>" + dishesSelected[i].name + "</h5> <p>" + dishesSelected[i].description + "</p> </div>  <div class='col-md-5'> <h5>Preperations</h5> <p>" + dishesSelected[i].description + "</p> </div> </div>");
       }      
    }
    
